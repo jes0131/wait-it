cdb395e97c1a2ca2efff0fbee43a44e8cf96c5bb
jes0131 <wkddmstn0131@gmail.com>
2025-11-03 12:19:44 +0900

    first commit

---
+++ src/main/java/com/jes/waitit/WaitItApplication.java
@@ -0,0 +1,13 @@
+package com.jes.waitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jes/waitit/WaitItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jes.waitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
