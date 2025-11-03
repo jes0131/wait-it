cea7ae856106dc7f015aa3c0ec5a22f3a27844e1
jes0131 <wkddmstn0131@gmail.com>
2025-11-03 14:56:25 +0900

    fix: health Get 요청 매핑

--- src/main/java/com/jes/waitit/domain/health/controller/HealthController.java
+++ src/main/java/com/jes/waitit/domain/health/controller/HealthController.java
@@ -2,12 +2,14 @@ package com.jes.waitit.domain.health.controller;
 
 import com.jes.waitit.global.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/health")
 public class HealthController {
+    @GetMapping
     public ResponseEntity<ApiResponse<String>> health() {
         return ResponseEntity.ok(ApiResponse.success("im ok genchana"));
     }
