a7f5e029899e092a34be859a5b1dd9a31f0d2f4a
jes0131 <wkddmstn0131@gmail.com>
2025-11-05 09:05:21 +0900

    feat: findByUsername 추가

--- src/main/java/com/jes/waitit/domain/user/service/UserService.java
+++ src/main/java/com/jes/waitit/domain/user/service/UserService.java
@@ -2,6 +2,8 @@ package com.jes.waitit.domain.user.service;
 
 import com.jes.waitit.domain.user.entity.User;
 import com.jes.waitit.domain.user.respository.UserRepository;
+import com.jes.waitit.global.exception.CustomException;
+import com.jes.waitit.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,11 @@ public class UserService {
 
     public boolean existsByUsername(String username) {
         return userRepository.existsByUsername(username);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     public void save(User user) {
