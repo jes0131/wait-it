194a890acd2da7822100a25a841437886c0697cd
jes0131 <wkddmstn0131@gmail.com>
2025-11-29 12:03:19 +0900

    fix: 쿼리 사소한 오류 수정
    - = 제거

--- src/main/java/com/jes/waitit/domain/submission/repository/SubmissionRepository.java
+++ src/main/java/com/jes/waitit/domain/submission/repository/SubmissionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-    @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.submission_state = IN ('CANCELED', 'RESOLVED') AND s.is_deleted = false", nativeQuery = true)
+    @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.submission_state IN ('CANCELED', 'RESOLVED') AND s.is_deleted = false", nativeQuery = true)
     Optional<Integer> findLastProcessedWaitingNumByReservationId(Long reservationId);
 
     @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.is_deleted = false", nativeQuery = true)
