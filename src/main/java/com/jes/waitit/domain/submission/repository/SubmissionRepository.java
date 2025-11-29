06f1139da0f56aac62128fe7f8564542b940c386
jes0131 <wkddmstn0131@gmail.com>
2025-11-29 11:55:26 +0900

    fix: merge 오류로 바뀐 내용 수정
    - SubmissionState RESOLVED 추가
    - 가장 마지막으로 처리된 대기 번호 조회 메서드 이름 및 쿼리 수정

--- src/main/java/com/jes/waitit/domain/submission/repository/SubmissionRepository.java
+++ src/main/java/com/jes/waitit/domain/submission/repository/SubmissionRepository.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-    @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.submission_state = PENDING", nativeQuery = true)
-    Optional<Integer> findMaxWaitingNumByReservationIdAndSubmissionStateIsPending(Long reservationId);
+    @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.submission_state = IN ('CANCELED', 'RESOLVED') AND s.is_deleted = false", nativeQuery = true)
+    Optional<Integer> findLastProcessedWaitingNumByReservationId(Long reservationId);
 
     @Query(value = "SELECT MAX(s.waiting_num) FROM submissions s WHERE s.reservation_id = :reservationId AND s.is_deleted = false", nativeQuery = true)
     Optional<Integer> findLastWaitingNumByReservationIdAndIsDeletedFalse(Long reservationId);
