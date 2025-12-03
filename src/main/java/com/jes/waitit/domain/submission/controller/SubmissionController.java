916733b1ad48518fd474e72c70faed3e12fcdf95
jes0131 <wkddmstn0131@gmail.com>
2025-12-03 14:07:04 +0900

    docs: `@Tag` 추가

--- src/main/java/com/jes/waitit/domain/submission/controller/SubmissionController.java
+++ src/main/java/com/jes/waitit/domain/submission/controller/SubmissionController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/submission")
 @RequiredArgsConstructor
+@Tag(name = "submission", description = "제출 관리 API")
 public class SubmissionController {
     private final SubmissionService submissionService;
 
